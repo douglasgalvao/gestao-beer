5ee530128ae153efb5bb287e6dddd0525d6bb052
douglas <douglasgmachado98@gmail.com>
2023-12-06 16:00:55 -0300

    feat: add FadeIn in components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Acoes } from './home-data';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 export interface NovaAcao {
   linkRouter: string,
   iconName: string,
@@ -9,7 +9,15 @@ export interface NovaAcao {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('700ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ]
 })
 export class HomeComponent {
 
--- src/app/vendas/vendas.component.ts
+++ src/app/vendas/vendas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogInformationVendaComponent } from './dialog-information-venda/dialog-information-venda.component';
 import { DialogoService } from '../service/dialogo.service';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 export interface ProdutoElement {
   nome: string,
   quantidade: number,
@@ -25,7 +25,15 @@ export interface VendaElement {
 @Component({
   selector: 'app-vendas',
   templateUrl: './vendas.component.html',
-  styleUrls: ['./vendas.component.scss']
+  styleUrls: ['./vendas.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('700ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ]
 })
 export class VendasComponent implements OnInit {
   constructor(private dialog: MatDialog, private dialogService: DialogoService) { }
@@ -39,7 +47,9 @@ export class VendasComponent implements OnInit {
   abrirDialog(venda: VendaElement) {
     this.dialog.open(DialogInformationVendaComponent, {
       width: '60%',
-      height: '60%'
+      height: '60%',
+      enterAnimationDuration: '500ms',
+      exitAnimationDuration: '400ms'
     })
     this.dialogService.setVendaData(venda);
   }
