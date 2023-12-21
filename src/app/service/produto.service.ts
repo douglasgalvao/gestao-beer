a44286d03b374c6233d461f67f07d5ec43cee241
douglas <douglasgmachado98@gmail.com>
2023-12-21 04:40:38 -0300

    feat: create produto service

---
+++ src/app/service/produto.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/enviroment';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  constructor(private http: HttpClient) { }
+
+  apiUrl = environment.apiUrl;
+  getProdutos(): Observable<any> {
+    return this.http.get(this.apiUrl + '/produto');
+  }
+}
