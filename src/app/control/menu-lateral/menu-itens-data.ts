2ca70425613dec03108f36148b06b2be97813941
douglas <douglasgmachado98@gmail.com>
2023-12-21 04:40:31 -0300

    add: Despesas and Estoque Modules to do!!

--- src/app/control/menu-lateral/menu-itens-data.ts
+++ src/app/control/menu-lateral/menu-itens-data.ts
@@ -3,6 +3,8 @@ export const Menu: MenuLateralItens[] = [
     { nome: "Home", linkRouter: "home", iconName: "home" },
     { nome: "Vendas", linkRouter: "vendas", iconName: "sell" },
     { nome: "Produtos", linkRouter: "produtos", iconName: "fastfood" },
+    { nome: "Despesas", linkRouter: "despesas", iconName: "attach_money" },
+    { nome: "Estoque", linkRouter: "estoque", iconName: "credit_card" },
     { nome: "Fornecedores", linkRouter: "fornecedores", iconName: "shopping_cart" },
     { nome: "Configurações", linkRouter: "config", iconName: "settings" },
 ]
