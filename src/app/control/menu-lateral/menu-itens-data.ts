ffa0695872c04906f7a80706ab55029577405b5e
douglas <douglasgmachado98@gmail.com>
2023-12-28 18:17:05 -0300

    add: clientes route

--- src/app/control/menu-lateral/menu-itens-data.ts
+++ src/app/control/menu-lateral/menu-itens-data.ts
@@ -1,6 +1,7 @@
 import { MenuLateralItens } from "./menu-lateral.component";
 export const Menu: MenuLateralItens[] = [
     { nome: "Home", linkRouter: "home", iconName: "home" },
+    { nome: "Clientes", linkRouter: "clientes", iconName: "person" },
     { nome: "Vendas", linkRouter: "vendas", iconName: "sell" },
     { nome: "Produtos", linkRouter: "produtos", iconName: "fastfood" },
     { nome: "Despesas", linkRouter: "despesas", iconName: "attach_money" },
