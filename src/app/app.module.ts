096225357e45d6c8a0307cbf53b2b42585f23f6a
douglas <douglasgmachado98@gmail.com>
2024-01-04 05:24:21 -0300

    add: including login and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
-import { HomeModule } from './home/home.module';
-import { MenuBarComponent } from './control/menu-bar/menu-bar.component';
-import { MenuLateralComponent } from './control/menu-lateral/menu-lateral.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+import { SharedModule } from './shared/shared.module';
+import { ContentRoutingModule } from './content/content-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent,
-    MenuLateralComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ContentRoutingModule,
+    SharedModule,
     MaterialModule,
-    HomeModule
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
