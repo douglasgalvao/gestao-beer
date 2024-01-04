14ca499f88878827761225295143d64f06d9cc15
douglas <douglasgmachado98@gmail.com>
2024-01-04 15:38:42 -0300

    add: verify if user is authenticated when init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
+import { NotificationService } from 'src/app/service/notification.service';
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { MatDialog } from '@angular/material/dialog';
 import { NovaVendaDialogComponent } from '../vendas/nova-venda-dialog/nova-venda-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -24,11 +26,18 @@ export class HomeComponent implements OnInit {
 
   // dataSource = new MatTableDataSource(this.vendas);
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog,
+    private notificationService: NotificationService,
+    private route: Router) {
     this.dialog = dialog;
   }
 
   ngOnInit(): void {
+    this.notificationService.isAuthenticated$.subscribe(data => {
+      if (!data) {
+        this.route.navigate(['/login'])
+      }
+    });
   }
 
   abrirDialogVenda() {
