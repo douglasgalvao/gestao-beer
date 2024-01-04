304eb6f9b479be284edaaa34fdaa446dca30b4a3
douglas <douglasgmachado98@gmail.com>
2024-01-04 05:37:04 -0300

    feat: add auth service

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,38 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterLink, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of, Observable, shareReplay } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpCliente: HttpClient, private route: Router) { }
+
+  efetuarLogin(login: string, password: string) {
+
+    this.httpCliente.post('http://localhost:8080/auth/login', { login, password }).subscribe(
+      (data: any) => {
+        localStorage.setItem('token', data.token);
+        this.route.navigate(['content/home']);
+      }
+    );
+
+  }
+
+
+  isAuthenticated(): Observable<boolean> {
+    let token = localStorage.getItem('token');
+    if (token != null) {
+      return this.httpCliente.get<boolean>('http://localhost:8080/auth/validate', {
+        headers: {
+          'Authorization': token!
+        }
+      }).pipe(
+        shareReplay(1)
+      );
+    } else {
+      return of(false);
+    }
+  }
+}
