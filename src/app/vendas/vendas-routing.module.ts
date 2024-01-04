8febc58ffbabb3c82f3d43903930f12e21e26806
douglas <douglasgmachado98@gmail.com>
2024-01-04 15:39:32 -0300

    code

--- src/app/vendas/vendas-routing.module.ts
+++ src/app/vendas/vendas-routing.module.ts
@@ -1,15 +1,16 @@
 
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VendasComponent } from './vendas.component';
 import { HomeComponent } from '../home/home.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 
 
 const routes: Routes = [
   { path: "", redirectTo: "/vendas/vendas", pathMatch: 'full' },
-  { path: "vendas", component: VendasComponent }
+  { path: "vendas", component: VendasComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
