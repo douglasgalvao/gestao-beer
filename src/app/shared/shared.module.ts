e4eb8f05b7b8cb61997bbce2f710ffcb23c3f7c2
douglas <douglasgmachado98@gmail.com>
2024-01-06 15:25:48 -0300

    feat: add page-not-found but it's not used yet, nowadays it's redirected to home when not found the path way

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from '../material-module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     ToolbarComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     CommonModule,
