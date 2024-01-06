b7a863b52349240c524edea3bca7962ad2f72302
douglas <douglasgmachado98@gmail.com>
2024-01-06 15:17:46 -0300

    chore: comment modules that is not implemented

--- src/app/content/menu-lateral/menu-itens-data.ts
+++ src/app/content/menu-lateral/menu-itens-data.ts
@@ -1,11 +1,12 @@
 import { MenuLateralItens } from "./menu-lateral.component";
 export const Menu: MenuLateralItens[] = [
-    { nome: "Home", linkRouter: "/content/home/home", iconName: "home" },
-    { nome: "Clientes", linkRouter: "/content/clientes", iconName: "person" },
-    { nome: "Vendas", linkRouter: "/content/vendas/vendas", iconName: "sell" },
-    { nome: "Produtos", linkRouter: "/content/produtos/produtos", iconName: "fastfood" },
-    { nome: "Despesas", linkRouter: "/content/despesas/despesas", iconName: "attach_money" },
-    { nome: "Estoque", linkRouter: "/content/estoque/estoque", iconName: "credit_card" },
-    { nome: "Fornecedores", linkRouter: "/content/fornecedores/fornecedores", iconName: "shopping_cart" },
-    { nome: "Configurações", linkRouter: "config", iconName: "settings" },
+    { nome: "Home", linkRouter: "/auth/content/home", iconName: "home" },
+    // { nome: "Clientes", linkRouter: "/auth/content", iconName: "person" },
+    { nome: "Vendas", linkRouter: "/auth/content/vendas", iconName: "sell" },
+    { nome: "Produtos", linkRouter: "/auth/content/produtos", iconName: "fastfood" },
+    // { nome: "Despesas", linkRouter: "/auth/content/despesas", iconName: "attach_money" },
+    { nome: "Estoque", linkRouter: "/auth/content/estoque", iconName: "credit_card" },
+    { nome: "Fornecedores", linkRouter: "/auth/content/fornecedores", iconName: "shopping_cart" },
+    // { nome: "Configurações", linkRouter: "config", iconName: "settings" },
 ]
+
