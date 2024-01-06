fb32e02314912892f633d5671d0a345f15064fa8
douglas <douglasgmachado98@gmail.com>
2024-01-06 15:24:50 -0300

    chore: add func API produto cosmos but it's not used anymore

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ProdutoFileIdResponse } from '../produtos/produtos.component';
+import { ProdutoAPIResponse, ProdutoFileIdResponse } from '../produtos/produtos.component';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,7 @@ export class ProdutoService {
   apiUrl = environment.apiUrl;
 
   cadastrarNovoProduto(produto: ProdutoElement): Observable<ProdutoElement> {
+    
     return this.http.post<ProdutoElement>(this.apiUrl + '/produto', {
       nome: produto.nome,
       categoria_Produto: produto.categoria_Produto,
@@ -58,7 +59,6 @@ export class ProdutoService {
     });
   }
 
-
   verificarProdutoJaExiste(nome: string): Observable<boolean> {
     return this.http.get<boolean>(this.apiUrl + '/produto/nome/' + nome, {
       headers: new HttpHeaders({
@@ -70,33 +70,17 @@ export class ProdutoService {
 
   salvarImagemProduto(fotoProduto: File): Observable<ProdutoFileIdResponse> {
     let endpoint = "http://localhost:8080/produto/upload";
-
     // Crie um objeto FormData e adicione os campos necessários
     const formData = new FormData();
     formData.append('img', fotoProduto);
 
-    console.log(formData);
     // Faça a requisição usando o HttpClient
     return this.http.post<ProdutoFileIdResponse>(endpoint, formData, {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token')
       })
     });
   }
-
-  // deletarImagemProduto(imgUrl: string): Observable<any> {
-  //   const headers = new HttpHeaders({
-  //     'Authorization': `Uploadcare.Simple ${this.apiSecretKey}`,
-  //     'Accept': 'application/vnd.uploadcare-v0.7+json'
-  //   });
-  //   imgUrl = imgUrl.replace('https://ucarecdn.com/', 'https://ucarecdn.com/files/');
-  //   imgUrl = imgUrl + '/storage/';
-  //   const params = new HttpParams()
-  //     .set('UPLOADCARE_STORE', 'auto');
-
-  //   return this.http.delete<any>(imgUrl, { headers, params });
-  // }
 
   getProdutoById(id: number): Observable<ProdutoElement> {
     return this.http.get<ProdutoElement>(this.apiUrl + '/produto/' + id, {
@@ -119,7 +103,6 @@ export class ProdutoService {
   deleteProduto(id: number): Observable<ProdutoElement> {
     return this.http.delete<ProdutoElement>(this.apiUrl + '/produto/' + id, {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token')
       })
     });
@@ -133,5 +116,15 @@ export class ProdutoService {
       })
     });
   }
+
+  getProdutoAPIByCode(codBarras: string): Observable<ProdutoAPIResponse> {
+    return this.http.get<ProdutoAPIResponse>('http://localhost:8080/produto/find/' + codBarras, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      })
+    });
+  }
+
 
 }
