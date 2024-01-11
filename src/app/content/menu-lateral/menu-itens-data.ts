fb28d1c111c181a24b29552be61596e4e55bec72
douglas <douglasgmachado98@gmail.com>
2024-01-11 01:12:29 -0300

    chore: remova links that not used and add Comandas

--- src/app/content/menu-lateral/menu-itens-data.ts
+++ src/app/content/menu-lateral/menu-itens-data.ts
@@ -1,12 +1,9 @@
 import { MenuLateralItens } from "./menu-lateral.component";
 export const Menu: MenuLateralItens[] = [
     { nome: "Home", linkRouter: "/auth/content/home", iconName: "home" },
-    // { nome: "Clientes", linkRouter: "/auth/content", iconName: "person" },
     { nome: "Vendas", linkRouter: "/auth/content/vendas", iconName: "sell" },
     { nome: "Produtos", linkRouter: "/auth/content/produtos", iconName: "fastfood" },
-    // { nome: "Despesas", linkRouter: "/auth/content/despesas", iconName: "attach_money" },
     { nome: "Estoque", linkRouter: "/auth/content/estoque", iconName: "credit_card" },
-    { nome: "Fornecedores", linkRouter: "/auth/content/fornecedores", iconName: "shopping_cart" },
-    // { nome: "Configurações", linkRouter: "config", iconName: "settings" },
+    { nome: "Comandas", linkRouter: "/auth/content/comanda", iconName: "assignment" },
 ]
 
