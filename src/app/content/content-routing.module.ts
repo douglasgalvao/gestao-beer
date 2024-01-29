5dfd0673a3c36e94b5eda9e8f89cff5af0630c2b
douglas <douglasgmachado98@gmail.com>
2024-01-29 20:07:24 -0300

    chore: add comanda and remove despesas and fornecedores link path

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -15,8 +15,9 @@ const routes: Routes = [
       { path: "vendas", loadChildren: () => import('../vendas/vendas.module').then(module => module.VendasModule) },
       { path: "produtos", loadChildren: () => import('../produtos/produtos.module').then(module => module.ProdutosModule) },
       { path: "estoque", loadChildren: () => import('../estoque/estoque.module').then(module => module.EstoqueModule) },
-      { path: "despesas", loadChildren: () => import('../despesas/despesas.module').then(module => module.DespesasModule) },
-      { path: "fornecedores", loadChildren: () => import('../fornecedores/fornecedores.module').then(module => module.FornecedoresModule) },
+      { path: "comanda", loadChildren: () => import('../comanda/comanda.module').then(module => module.ComandaModule) },
+      // { path: "despesas", loadChildren: () => import('../despesas/despesas.module').then(module => module.DespesasModule) },
+      // { path: "fornecedores", loadChildren: () => import('../fornecedores/fornecedores.module').then(module => module.FornecedoresModule) },
       { path: "**", redirectTo: "/auth/content/home" }
     ]
   }
