7b977f439487449bee1fb6cf56a1160e7c1e50a8
douglas <douglasgmachado98@gmail.com>
2024-03-08 20:12:38 -0300

    create module comanda

--- src/app/comanda/comanda.module.ts
+++ src/app/comanda/comanda.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ComandaComponent } from './comanda.component';
 import { MaterialModule } from '../material-module';
 import { ComandaRoutingModule } from './comanda-routing.module';
+import { CardComandaComponent } from './card-comanda/card-comanda.component';
+import { CardCriarComandaComponent } from './card-criar-comanda/card-criar-comanda.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    ComandaComponent
+    ComandaComponent,
+    CardComandaComponent,
+    CardCriarComandaComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    ComandaRoutingModule
+    ComandaRoutingModule,
+    SharedModule
   ]
 })
 export class ComandaModule { }
