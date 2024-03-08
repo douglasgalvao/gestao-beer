4f65a7e77f910e3921100307c535637421d1bf3c
douglas <douglasgmachado98@gmail.com>
2024-03-08 20:24:30 -0300

    feat: create init criar-card-comanda

---
+++ src/app/comanda/card-criar-comanda/card-criar-comanda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-criar-comanda',
+  templateUrl: './card-criar-comanda.component.html',
+  styleUrls: ['./card-criar-comanda.component.scss']
+})
+export class CardCriarComandaComponent {
+
+}
