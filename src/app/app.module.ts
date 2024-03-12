c7d3a3cfa4b2052215eadc726dc0623df87018ff
douglas <douglasgmachado98@gmail.com>
2024-03-12 17:46:47 -0300

    comum commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-import { ContentComponent } from './content/content.component';
 import { SharedModule } from './shared/shared.module';
 import { ContentRoutingModule } from './content/content-routing.module';
 
