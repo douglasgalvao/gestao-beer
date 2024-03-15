6bec707833ae3dc0eb4a283abf37076b8b9a353e
douglas <douglasgmachado98@gmail.com>
2024-03-14 23:32:50 -0300

    redirecting correctly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: 'full' },
+  { path: "", redirectTo: "auth/content/home", pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'auth', loadChildren: () => import('./content/content.module').then(m => m.ContentModule) },
   { path: '**', redirectTo: 'auth/content/home' }
